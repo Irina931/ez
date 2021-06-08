7048c43c3e4ec9768e16ce7a65463cbb17bc30d5
Irina931 <Irishkakara2718@gmail.com>
2021-06-08 22:47:18 +0300

    add Ira

---
+++ Ira/Ira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
